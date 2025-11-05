3f459a1e8257161fc1d76a32fdaee9da91729bfa
Antón RS <arodriguezseoane@danielcastelao.org>
2025-11-05 17:14:30 +0100

    Implement 'Persoa' class with validation methods
    Created 'Persoa' class with properties and methods for validation.

---
+++ Clase/persoa.py
@@ -0,0 +1,38 @@
+'''Crear clase persoa onde teño propiedades nome, idade, dni, direccion e nacemento.
+
+Añadir tres funciones:
+
+aCadea()
+comprobarIdade(edade)
+comprobarDNI(dni)
+'''
+class Persoa:
+    def __init__(self, nome, idade, dni, direccion, nacionalidade):
+        self.nome = nome
+        self.idade = idade
+        self.dni = dni
+        self.direccion = direccion
+        self.nacionalidade = nacionalidade
+
+    def aCadea(self):
+        cadea = f"Nome: {self.nome}\nIdade: {self.idade}\nDNI: {self.dni}\nDirección: {self.direccion}\nNacionalidade: {self.nacionalidade}"
+        return cadea
+
+    def comprobarIdade(self,idade):
+        if idade > 110:
+            return 0
+        elif idade < 0:
+            return 0
+        else:
+            return idade
+
+    def comprobarDNI(self,dni):
+        if len(dni) == 9 and str(dni[-1]).isupper() and dni[:8].isdigit():
+            return dni
+        else:
+            return "XXXXXXXXX"
+
+
+    def __str__(self):
+        return self.aCadea()
+
