ea3ced28a41dc94a415f7b33ef0377ae6721e96f
arseoane <arodriguezseoane@danielcastelao.org>
2025-11-07 10:11:03 +0100

    k

---
+++ Clase/circulo.py
@@ -0,0 +1,11 @@
+import math
+
+class Circulo:
+    def __init__(self, radio):
+        self.radio = radio
+
+    def obterDiametro(self):
+        return self.radio * 2
+
+    def perimetro(radio):
+        return 2 * math.pi * radio
--- Clase/main.py
+++ Clase/main.py
@@ -1,16 +1,3 @@
-'''Crear clase persoa onde teño propiedades nome, idade, dni, direccion e nacemento.
+from circulo import Circulo
 
-Añadir tres funciones:
-
-aCadea()
-comprobarIdade(edade)
-comprobarDNI(dni)
-'''
-
-from persoa import Persoa
-
-persoa1 = Persoa("Enrique",33,"47474747F","Av. Adriana Salte, 67", "Bolivia")
-
-print(persoa1.aCadea())
-print(persoa1.comprobarDNI(persoa1.dni))
-print(persoa1.comprobarIdade(persoa1.idade))
+print(Circulo.perimetro(10))
