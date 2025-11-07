e7b3cc795b38dfdf1af3075fdfebb84339949308
arseoane <arodriguezseoane@danielcastelao.org>
2025-11-07 10:12:40 +0100

    k

--- Clase/circulo.py
+++ Clase/circulo.py
@@ -4,8 +4,8 @@ class Circulo:
     def __init__(self, radio):
         self.radio = radio
 
-    def obterDiametro(self):
-        return self.radio * 2
+    def obterDiametro(radio):
+        return radio * 2
 
     def perimetro(radio):
         return 2 * math.pi * radio
--- Clase/main.py
+++ Clase/main.py
@@ -1,3 +1,4 @@
 from circulo import Circulo
 
+print(Circulo.obterDiametro(10))
 print(Circulo.perimetro(10))
