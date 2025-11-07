8b17d6c7bec7243d7fec0272ddd938f644350517
arseoane <arodriguezseoane@danielcastelao.org>
2025-11-07 17:25:37 +0100

    Completar exercicios

--- Pruebas/main.py
+++ Pruebas/main.py
@@ -1,4 +1,5 @@
 from persoa import Persoa
+from coche import Coche
 
 p1 = Persoa("Antonio",109,"12345678Z")
 
@@ -6,3 +7,4 @@ Persoa.cumprirAno(p1.idade)
 print(p1.idade)
 Persoa.cumprirAno(p1.idade)
 print(p1.idade)
+print("Coche:", Coche.mostrar_info("ferrari","450","2023"))
