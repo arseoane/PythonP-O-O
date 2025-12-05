55066dfd6b88048f693aa5ae5b91824fcb8afa1f
arseoane <arodriguezseoane@danielcastelao.org>
2025-12-05 15:58:40 +0100

    Completar exercicios

--- Pruebas/main.py
+++ Pruebas/main.py
@@ -1,6 +1,6 @@
 from horastest import Horas
 
-horas1 = Horas(23, 48, 20)
+horas1 = Horas(23, 84, 20)
 print(horas1.mostrarFormato12Horas())
 
 horas1._Horas__asignacionHoraColeccion(3203)
@@ -15,7 +15,7 @@ horas2 = Horas(23, 48, 20)
 
 horas2._Horas__asignacionHoraColeccion(23)
 
-horas1._Horas__asignacionSegundoColeccion(10000)
+horas1._Horas__asignacionSegundoColeccion(100)
 
 print(horas1.mostrarSegundos())
 
@@ -23,4 +23,6 @@ print(horas2.mostrarSegundos())
 
 print(isinstance(horas1, Horas))
 
-print(horas2.mostrarHoras())
+print(horas2.setHoras(932039))
+
+horas2._Horas__asignacionHoraColeccion(23)
