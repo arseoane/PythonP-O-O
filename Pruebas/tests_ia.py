f7e4cb5b06d3eed5207a38e10da5b03be75d0b9f
arseoane <arodriguezseoane@danielcastelao.org>
2025-12-10 16:43:09 +0100

    Completar exercicios

--- Pruebas/tests_ia.py
+++ Pruebas/tests_ia.py
@@ -1,79 +1,44 @@
-class Vehiculo:
-    def __init__(self, marca, modelo, ano):
-        self.marca = marca
-        self.modelo = modelo
+class Libro:
+    def __init__(self, titulo, autor, ano, numPags, valor):
+        self.titulo = titulo
+        self.autor = autor
         self.ano = ano
+        self.numPags = numPags
+        self.valor = valor
 
-    def info_basica(self):
-        print(f"Marca: {self.marca}\nModelo: {self.modelo}\nAño: {self.ano}")
+    def amosarLibro(self):
+        return f'''Título: {self.titulo}
+Autor: {self.autor}
+Ano: {self.ano}
+Núm. Pags: {self.numPags}
+Valoración: {self.valor}'''
 
+class Coche:
+    def __init__(self):
+        self.velocidade = 0
 
-class Coche(Vehiculo):
-    def __init__(self, marca, modelo, ano, velocidad=0,combustible=50):
-        super().__init__(marca,modelo,ano)
-        self.__encendido = False
-        self.__velocidad = velocidad
-        self.__combustible = combustible
+    def getVelocidade(self):
+        return f"Velocidade: {self.velocidade} km/h"
 
-    def encender(self):
-        self.__encendido = True
-        print("El coche está encendido.")
+    def acelerar(self, suma):
+        self.velocidade += suma
 
-    def apagar(self):
-        self.__encendido = False
-        print("El coche está apagado.")
+    def frenar(self, resta):
+        self.velocidade -= resta
 
-    def acelerar(self, incremento):
-        if self.__combustible <= 0:
-            print("¡Combustible agotado!")
-        else:
-            self.__velocidad += incremento
-            print(f"Acelerando... Velocidad actual: {self.__velocidad} km/h")
-            self.__combustible -= incremento
-
-    def frenar(self, decremento):
-        self.__velocidad -= decremento
-        print(f"Frenando... Velocidad actual: {self.__velocidad} km/h")
-
-    def repostar(self, litros):
-        self.__combustible += litros
-        print(f"Repostando... Combustible actual: {self.__combustible} litros")
-
-    def info(self):
-        print(f"Marca: {self.marca}\nModelo: {self.modelo}\nAño: {self.ano}")
-        if (self.__encendido == False):
-            print("Estado: Apagado")
-        else:
-            print("Estado: Encendido")
-        print(f"Velocidad: {self.__velocidad} km/h\nCombustible: {self.__combustible} litros")
+class Conta:
+    def __init__(self, nome, numconta, tipointerese, saldo):
+        self.nome = nome
+        self.numconta = numconta
+        self.tipointerese = tipointerese
+        self.saldo = saldo
 
     @property
-    def leerVel(self):
-        return str(self.__velocidad)
+    def nome(self):
+        return self.nome
 
-class Moto(Vehiculo):
-    def __init__(self, marca, modelo, ano, cilindrada):
-        super().__init__(marca, modelo, ano)
-        self.cilindrada = cilindrada
-
-    def hacer_caballito(self):
-        print("¡Wheeee! Haciendo caballito 🏍️")
+    def ingreso(self, valor):
+        self.saldo += valor
 
     def info(self):
-        print(f"Marca: {self.marca}\nModelo: {self.modelo}\nAño: {self.ano}\nCilindrada: {self.cilindrada}cc")
-
-class Camion(Vehiculo):
-    def __init__(self, marca, modelo, ano, carga_maxima):
-        super().__init__(marca,modelo,ano)
-        self.carga_maxima = carga_maxima
-        self.carga_actual = 0
-
-    def cargar(self, kg):
-        self.carga_actual += kg
-        if self.carga_maxima < self.carga_actual:
-            print("¡Carga máxima excedida!")
-        else:
-            print(f"Cargando... Carga actual: {self.carga_actual} kg")
-
-    def info(self):
-        print(f"Marca: {self.marca}\nModelo: {self.modelo}\nAño: {self.ano}\nCarga máxima: {self.carga_maxima} kg\nCarga actual: {self.carga_actual} kg")
+        return f'''Nome: {self.nome}\nNúmero de conta: {self.numconta}\nTipo de interese: {self.tipointerese}\nSaldo: {self.saldo}'''
