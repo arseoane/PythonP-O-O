ef2aa0f6fca6081a5757f4bb28d4372dad21bf53
arseoane <arodriguezseoane@danielcastelao.org>
2026-01-08 13:28:47 +0100

    Calculadora binaria

--- Clase/main.py
+++ Clase/main.py
@@ -3,3 +3,10 @@ from calculadorabinaria import *
 calc = CalculadoraBinaria(10, 5)
 print(calc.operacion('+'))
 print(calc.operacion('/'))
+
+errorcalc = CalculadoraBinaria(10, 0)
+
+print(errorcalc.operacion('-'))
+print(errorcalc.operacion('*'))
+print(errorcalc.operacion('/'))
+print(errorcalc.operacion('@'))
